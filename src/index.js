b8da9ea697e9612babb877eaca95929a3f23af76
ILLIA DOBROVOLSKYI <illia.gopro@gmail.com>
2022-07-16 16:25:35 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -37,10 +37,52 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {  
+    count = 1;
+    let tt = '';
+    for(let i=0; i<expr.length-1; i+=2){
+        //console.log(i, expr.length)
+        
+        if(expr[i] === '1' && expr[i+1] === '0' ){
+            tt +='.';                     
+        } else if(expr[i] === '1' && expr[i+1] === '1'){
+            tt +='-';                    
+        } else if (expr[i] === '0' && expr[i+1] === '0'){
+            tt +=' ';                    
+        }
+       
+        if (expr[i] === '*') {        
+            
+            tt += ' ';          
+        }
+        if(count === 5){
+            tt += ',';
+            count = 0 
+        }
+        count++
+        //console.log(count)
+    }
+
+    const decodeLetter = letter => {      
+      
+        return MORSE_TABLE[letter.trim()]
+        
+      }
+      
+      const decodeWord = word => {
+        //console.log(word)      
+        return word.split(',').map(decodeLetter).join('');
+      }
+      
+      const decodeMorse = morseCode => {
+        return morseCode.trim().split('     ').map(decodeWord).join(' ');
+      }    
+
+        return decodeMorse(tt)
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode('10111111111111101010001110101000101011101010111111101010101011111111101110101010'))
